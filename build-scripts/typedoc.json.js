98403011ac9f0b8085605b19f0754579f7644516
FBplus <1298782235@qq.com>
2021-05-28 21:44:56 +0800

    update docs

---
+++ build-scripts/typedoc.json.js
@@ -0,0 +1,11 @@
+/** @type {import("typedoc").TypeDocOptions} */
+
+const config = {
+    entryPoints: ["src/index.ts"],
+    excludeExternals: false,
+    excludePrivate: false,
+    excludeProtected: false,
+    hideGenerator: true
+};
+
+module.exports = config;
